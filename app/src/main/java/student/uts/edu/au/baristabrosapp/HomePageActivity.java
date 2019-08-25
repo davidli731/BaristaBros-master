ac17f0fa631e9e4915efa49c6ef07a0c5b65d010
David Li <37282826+davidli731@users.noreply.github.com>
2019-08-25 20:59:08 +1000

    Updates to Menu Bar
    Dummy code to test list buttons to open new Activities,
    fixed bug where the back button would go out of the app rather than close menu,
    Menu now closes before moving to new Activity

--- app/src/main/java/student/uts/edu/au/baristabrosapp/HomePageActivity.java
+++ app/src/main/java/student/uts/edu/au/baristabrosapp/HomePageActivity.java
@@ -1,13 +1,49 @@
 package student.uts.edu.au.baristabrosapp;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class HomePageActivity extends AppCompatActivity {
+public class HomePageActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    //declare variables
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        NavigationView navView = findViewById(R.id.nav_view);
+        navView.setNavigationItemSelectedListener(this);
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
     }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.nav_sign_out) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            drawerLayout.closeDrawer(GravityCompat.START);
+            startActivity(intent);
+            finish();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
 }
